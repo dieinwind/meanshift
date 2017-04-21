151d48e6f2572c624b291e03c2263b222c4a2719
dieinwind <dieinwind@gmail.com>
2017-04-21 11:43:42 +0800

    meanshift

---
+++ MeanShift/MeanShift.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/OpenCV.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/legacy/legacy.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector> 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+
+}
